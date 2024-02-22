7ae655c8c88988893ceffa128c0fe5370e9ccb51
manoharbondalapati <manohar.bondalapati34@gmail.com>
2024-02-22 21:43:23 +0530

    updated

--- myquizapp/src/index.js
+++ myquizapp/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+//import './index.css';
 import HomePage from './HomePage';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import APP from './APP';
